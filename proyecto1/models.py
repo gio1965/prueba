e9f86fc9acfaf7f442324c63ccce221576af26a6
gio1965 <love1965@live.com.mx>
2018-10-02 10:42:18 -0500

    prueba

--- proyecto1/models.py
+++ proyecto1/models.py
@@ -10,5 +10,41 @@ class Alumno(models.Model):
 	def __str__(self):
 		return str(self.nombre)+ " tiene la edad de " + str(self.edad)
 
+class Estudiante(models.Model):
+	nombre = models.CharField('Ingresa nombre', max_length=60)
+	apellidoPaterno = models.CharField(max_length=60)
+	apellidoMaterno = models.CharField(max_length=60)
+	DNI = models.CharField(max_length=8)
+	fechaNacimiento = models.DateField()
+	sexos = (('F', 'Femenino'), ('M', 'Masculino'))
+	sexo = models.CharField(max_length=1, choices=sexos, default='M')
+	
+	def NombreCompleto(self):
+	cadena = "{0} {1}, {2}"
 
+		return cadena.format(self.nombre, self.apellidoPaterno)
+
+	def __str__(self):
+
+		return self.NombreCompleto()
+
+class Curso(models.Model):
+	Nombre = models.CharField(max_length=50)
+	Credito = models.PositiveSmallIntegerField()
+	Estado = models.models.BooleanField(default=True)
+
+	def __str__(self):
+
+		return "{0} ({1})".format(self.Nombre, self,Credito)
+
+class Matricula(models.Model):
+	Estudiante = models.ForeignKey(Estudiante null=False, blank=False, on_delete=models.CASCADE)
+	Curso = models.ForeignKey(Curso null=False, blank=False, on_delete=models.CASCADE)
+	fechaMatricula = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		cadena = "{0} => {1}"
+		return cadena.format(self.Estudiante, self.Curso.Nombre)
+	
+		
 # Create your models here.
